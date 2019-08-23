7fb796fb1d92c9657d92bbe5ee53ffde21f4919c
hexiandong <1170833484@qq.com>
2019-08-23 14:01:06 +0800

    rename

--- test/tx/deep_link.js
+++ test/tx/deep_link.js
@@ -1,29 +1,80 @@
+import {assert} from 'chai'
 import LemoTx from '../../lib/index'
 
 describe('create_deep_link', () => {
-    it('create_deep_link', () => {
+    it('empty_config', () => {
         const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D')
+    })
+
+    it('chainID_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+            chainID: 100,
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=100&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D')
+    })
+    it('type_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
             type: 1,
-            chainID: 1,
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?ty=1&c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D')
+    })
+    it('version_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
             version: 2,
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&v=2&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D')
+    })
+    it('to_config', () => {
+        const txConfig = {
             from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
             to: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
-            toName: 'accouneName',
-            gasPayer: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
-            gasPrice: 2345645465656,
-            gasLimit: 1342,
-            amount: 100010000000000000000,
-            data: '0x3gsdsy',
-            expirationTime: 1516561000,
-            message: 'this is a example',
         }
-        for (const key in txConfig) {
-            const url = LemoTx.createDeepLink({[key]: txConfig[key]})
-            console.log(url)
-        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&t=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D')
     })
-    it('parse_deep_link', () => {
-        const txConfig = LemoTx.parseDeepLink('lemo://pay/tx?ty=1&c=1&v=2&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&t=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&tn=accouneName&p=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&gp=2345.645465656&gl=1342&a=100.01&d=0x3gsdsy&e=1516561&m=dGhpcyBpcyBhIGV4YW1wbGU&s%5B%5D=7sdu&s%5B%5D=5dt6eu&s%5B%5D=5dgye&gs%5B%5D=5hf65gf&gs%5B%5D=h65hd')
-        console.log(txConfig)
+    it('tn_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+            toName: 'Lemo1234',
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&tn=Lemo1234')
+    })
+    it('p_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+            gasPayer: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&p=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D')
+    })
+    it('gp_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+            gasPrice: 1234,
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&gp=0.000001234')
+    })
+    it('gl_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+            toName: 'Lemo1234',
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&tn=Lemo1234')
+    })
+    it('tn_config', () => {
+        const txConfig = {
+            from: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
+            toName: 'Lemo1234',
+        }
+        assert.equal(LemoTx.createDeepLink(txConfig), 'lemo://pay/tx?c=1&f=Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D&tn=Lemo1234')
+    })
+})
+
+describe('', () => {
+    it('', () => {
     })
 })
