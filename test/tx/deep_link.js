3478f154094ab7d05ccf76792cb9fa653840fe94
lnk <wyy102489@outlook.com>
2019-09-23 16:52:36 +0800

    fix(tx): fix deeplink gasPayerSigs parsing problem; parseDeepLink not return Tx object any more; stricter verification

--- test/tx/deep_link.js
+++ test/tx/deep_link.js
@@ -11,11 +11,22 @@ describe('deep_link', () => {
             sigs: dataSigs,
             gasPayerSigs: dataSigs,
         }
-        const stringifyResult = createDeepLink(txConfig)
-        const paseResult = parseDeepLink(stringifyResult)
-        assert.equal(paseResult.from, txConfig.from)
-        assert.equal(paseResult.data, txConfig.data)
+        const uri = createDeepLink(txConfig)
+        const paseResult = parseDeepLink(uri)
+        assert.equal(paseResult.chainID, txConfig.chainID)
+        assert.equal(paseResult.version, txConfig.version)
+        assert.equal(paseResult.type, txConfig.type)
+        assert.equal(paseResult.to, txConfig.to)
+        assert.equal(paseResult.toName, txConfig.toName)
+        assert.equal(paseResult.gasPrice, txConfig.gasPrice)
+        assert.equal(paseResult.gasLimit, txConfig.gasLimit)
         assert.equal(paseResult.amount.toString(), txConfig.amount)
+        assert.equal(paseResult.data, txConfig.data)
+        assert.equal(paseResult.expirationTime, txConfig.expirationTime - (txConfig.expirationTime % 1000))
+        assert.equal(paseResult.message, txConfig.message)
+        assert.equal(paseResult.from, txConfig.from)
+        assert.equal(paseResult.gasPayer, txConfig.gasPayer)
+        assert.deepEqual(paseResult.gasPayerSigs, txConfig.gasPayerSigs)
         assert.deepEqual(paseResult.sigs, txConfig.sigs)
     })
     // only has one sig in Array
@@ -26,8 +37,8 @@ describe('deep_link', () => {
             sigs: txInfo.gasAfterSign,
             gasPayerSigs: txInfo.gasAfterSign,
         }
-        const stringifyResult = createDeepLink(txConfig)
-        const paseResult = parseDeepLink(stringifyResult)
+        const uri = createDeepLink(txConfig)
+        const paseResult = parseDeepLink(uri)
         assert.deepEqual(paseResult.sigs, txConfig.sigs)
     })
     // txConfig have not from
@@ -36,10 +47,16 @@ describe('deep_link', () => {
             to: 'Lemo836BQKCBZ8Z7B7N4G4N4SNGBT24ZZSJQD24D',
             chainID: 100,
         }
-        const result = createDeepLink(txConfig)
+        const uri = createDeepLink(txConfig)
+        const parsedConfig = parseDeepLink(uri)
+        assert.deepEqual(txConfig, parsedConfig)
+    })
+    // txConfig have not from
+    it('uri_contains_invalid_property', () => {
+        const uri = 'lemo://pay/tx?c=100&t=123456'
         assert.throws(() => {
-            parseDeepLink(result)
-        }, errors.TXFieldCanNotEmpty('from'))
+            parseDeepLink(uri)
+        }, errors.InvalidAddress('123456'))
     })
     // txConfig is empty
     it('empty_config', () => {
@@ -49,7 +66,7 @@ describe('deep_link', () => {
         }, errors.TXFieldCanNotEmpty('txConfig'))
     })
     // Initial url error
-    it('url_start_error', () => {
+    it('uri_contains_invalid_prefix', () => {
         const url = 'demo://pay/tx?c=100&f=Lemo888888888888888888888888888888888888'
         assert.throws(() => {
             parseDeepLink(url)
--- test/tx/tx.js
+++ test/tx/tx.js
@@ -1,8 +1,19 @@
 import {assert} from 'chai'
+import BigNumber from 'bignumber.js'
+import {toBuffer} from 'lemo-utils'
 import Tx from '../../lib/tx/tx'
 import errors from '../../lib/errors'
 import {testPrivate, txInfos, chainID, testAddr, emptyTxInfo} from '../datas'
-import {TxType, MAX_TX_TO_NAME_LENGTH, TX_SIG_BYTE_LENGTH, TX_VERSION, TTTL, TX_DEFAULT_GAS_LIMIT, TX_DEFAULT_GAS_PRICE} from '../../lib/const'
+import {
+    TxType,
+    MAX_TX_TO_NAME_LENGTH,
+    TX_SIG_BYTE_LENGTH,
+    TX_VERSION,
+    TTTL,
+    TX_DEFAULT_GAS_LIMIT,
+    TX_DEFAULT_GAS_PRICE,
+    MAX_TX_MESSAGE_LENGTH,
+} from '../../lib/const'
 import Signer from '../../lib/tx/signer'
 
 describe('Tx_new', () => {
@@ -139,13 +150,15 @@ describe('Tx_signWith', () => {
 })
 describe('all config', () => {
     const tests = [
+        {field: 'chainID', configData: undefined, result: 0},
         {field: 'chainID', configData: 1},
         {field: 'chainID', configData: 100},
         {field: 'chainID', configData: '10000', result: 10000},
         {field: 'chainID', configData: 'abc', error: errors.TXMustBeNumber('chainID', 'abc')},
-        {field: 'chainID', configData: '', result: NaN},
-        {field: 'chainID', configData: 0, result: 0},
+        {field: 'chainID', configData: '', result: 0},
+        {field: 'chainID', configData: 0, error: errors.TXInvalidRange('chainID', 0, 1, 0xffff)},
         {field: 'chainID', configData: '0x10000', error: errors.TXInvalidRange('chainID', '0x10000', 1, 0xffff)},
+        {field: 'type', configData: undefined, result: 0},
         {field: 'type', configData: 0},
         {field: 'type', configData: 1},
         {field: 'type', configData: 0xff},
@@ -154,7 +167,8 @@ describe('all config', () => {
         {field: 'type', configData: 'abc', error: errors.TXMustBeNumber('type', 'abc')},
         {field: 'type', configData: -1, error: errors.TXInvalidRange('type', -1, 0, 0xffff)},
         {field: 'type', configData: 0x10000, error: errors.TXInvalidRange('type', 0x10000, 0, 0xffff)},
-        {field: 'version', configData: 0, result: TX_VERSION},
+        {field: 'version', configData: undefined, result: TX_VERSION},
+        {field: 'version', configData: 0, result: 0},
         {field: 'version', configData: 1},
         {field: 'version', configData: 0xff},
         {field: 'version', configData: '', result: TX_VERSION},
@@ -162,8 +176,10 @@ describe('all config', () => {
         {field: 'version', configData: 'abc', error: errors.TXMustBeNumber('version', 'abc')},
         {field: 'version', configData: -1, error: errors.TXInvalidRange('version', -1, 0, 0xff)},
         {field: 'version', configData: 0x100, error: errors.TXInvalidRange('version', 0x100, 0, 0xff)},
+        {field: 'to', configData: undefined, result: ''},
+        {field: 'to', configData: ''},
         {field: 'to', configData: 0x1, error: errors.TXInvalidType('to', 0x1, ['string'])},
-        {field: 'to', configData: '0x1', result: 'Lemo8888888888888888888888888888888888BW', error: errors.InvalidAddress('0x1')},
+        {field: 'to', configData: '0x1', error: errors.InvalidAddress('0x1')},
         {field: 'to', configData: 'lemobw'},
         {field: 'to', configData: 'lemob', error: errors.InvalidAddressCheckSum('lemob')},
         {
@@ -171,17 +187,77 @@ describe('all config', () => {
             configData: 'Lemo9A9JGWQT74H37PSB24RTH6YYHG6W3GCH3CJ8S',
             error: errors.InvalidAddressLength('Lemo9A9JGWQT74H37PSB24RTH6YYHG6W3GCH3CJ8S'),
         },
+        {field: 'toName', configData: undefined, result: ''},
+        {field: 'toName', configData: ''},
         {field: 'toName', configData: 'lemo'},
         {
             field: 'toName',
-            configData: '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789',
+            configData: new Array(101).fill(1).join(''),
             error: errors.TXInvalidMaxLength(
                 'toName',
-                '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789',
+                new Array(101).fill(1).join(''),
                 MAX_TX_TO_NAME_LENGTH,
             ),
         },
         {field: 'toName', configData: 'testname\ndads\n', error: errors.InvalidToName()},
+        {field: 'gasPrice', configData: undefined, result: new BigNumber(TX_DEFAULT_GAS_PRICE)},
+        {field: 'gasPrice', configData: 0, result: new BigNumber(0)},
+        {field: 'gasPrice', configData: 1, result: new BigNumber(1)},
+        {field: 'gasPrice', configData: 0xff, result: new BigNumber(0xff)},
+        {field: 'gasPrice', configData: '', result: new BigNumber(TX_DEFAULT_GAS_PRICE)},
+        {field: 'gasPrice', configData: '1', result: new BigNumber(1)},
+        {field: 'gasPrice', configData: 'abc', error: errors.TXMustBeNumber('gasPrice', 'abc')},
+        {field: 'gasPrice', configData: -1, error: errors.TXNegativeError('gasPrice')},
+        {field: 'gasPrice', configData: '-1', error: errors.TXMustBeNumber('gasPrice', '-1')},
+        {field: 'gasLimit', configData: undefined, result: TX_DEFAULT_GAS_LIMIT},
+        {field: 'gasLimit', configData: 0},
+        {field: 'gasLimit', configData: 1},
+        {field: 'gasLimit', configData: 0xff},
+        {field: 'gasLimit', configData: '', result: TX_DEFAULT_GAS_LIMIT},
+        {field: 'gasLimit', configData: '1', result: 1},
+        {field: 'gasLimit', configData: 'abc', error: errors.TXMustBeNumber('gasLimit', 'abc')},
+        {field: 'gasLimit', configData: -1, error: errors.TXNegativeError('gasLimit')},
+        {field: 'gasLimit', configData: '-1', error: errors.TXMustBeNumber('gasLimit', '-1')},
+        {field: 'amount', configData: undefined, result: new BigNumber(0)},
+        {field: 'amount', configData: 0, result: new BigNumber(0)},
+        {field: 'amount', configData: 1, result: new BigNumber(1)},
+        {field: 'amount', configData: 0xff, result: new BigNumber(0xff)},
+        {field: 'amount', configData: '', result: new BigNumber(0)},
+        {field: 'amount', configData: '1', result: new BigNumber(1)},
+        {field: 'amount', configData: 'abc', error: errors.TXMustBeNumber('amount', 'abc')},
+        {field: 'amount', configData: -1, error: errors.TXNegativeError('amount')},
+        {field: 'amount', configData: '-1', error: errors.TXMustBeNumber('amount', '-1')},
+        {field: 'expirationTime', configData: undefined, result: Math.floor(Date.now() / 1000) + TTTL},
+        {field: 'expirationTime', configData: 0, result: Math.floor(Date.now() / 1000) + TTTL},
+        {field: 'expirationTime', configData: 1},
+        {field: 'expirationTime', configData: 0xff},
+        {field: 'expirationTime', configData: '', result: Math.floor(Date.now() / 1000) + TTTL},
+        {field: 'expirationTime', configData: '1', result: 1},
+        {field: 'expirationTime', configData: 'abc', error: errors.TXMustBeNumber('expirationTime', 'abc')},
+        {field: 'expirationTime', configData: -1, error: errors.TXNegativeError('expirationTime')},
+        {field: 'expirationTime', configData: '-1', error: errors.TXMustBeNumber('expirationTime', '-1')},
+        {field: 'data', configData: undefined, result: ''},
+        {field: 'data', configData: ''},
+        {field: 'data', configData: null, result: ''},
+        {field: 'data', configData: 0, error: errors.TXInvalidType('data', 0, ['string', 'object'])},
+        {field: 'data', configData: 'abc', error: errors.TXMustBeNumber('data', 'abc')},
+        {field: 'data', configData: '0xabc'},
+        {field: 'data', configData: {}, result: '0x7b7d'},
+        {field: 'data', configData: {a: 12, b: [{c: 'a'}]}, result: '0x7b2261223a31322c2262223a5b7b2263223a2261227d5d7d'},
+        {field: 'data', configData: toBuffer(0x0123), result: '0x0123'},
+        {field: 'message', configData: undefined, result: ''},
+        {field: 'message', configData: ''},
+        {field: 'message', configData: 'testname\ndad<2j398!>\'😋'},
+        {field: 'message', configData: 1, error: errors.TXInvalidType('message', 1, ['string'])},
+        {
+            field: 'message',
+            configData: new Array(1025).fill(1).join(''),
+            error: errors.TXInvalidMaxLength(
+                'message',
+                new Array(1025).fill(1).join(''),
+                MAX_TX_MESSAGE_LENGTH,
+            ),
+        },
         {field: 'sigs', configData: [], result: []},
         {field: 'sigs', configData: ['0'], result: ['0x0']},
         {field: 'sigs', configData: ['1'], result: ['0x1']},
@@ -227,6 +303,28 @@ describe('all config', () => {
                 66,
             ),
         },
+        {field: 'from', configData: undefined, error: errors.TXFieldCanNotEmpty('from')},
+        {field: 'from', configData: '', error: errors.TXFieldCanNotEmpty('from')},
+        {field: 'from', configData: 0x1, error: errors.TXInvalidType('from', 0x1, ['string'])},
+        {field: 'from', configData: '0x1', error: errors.InvalidAddress('0x1')},
+        {field: 'from', configData: 'lemobw'},
+        {field: 'from', configData: 'lemob', error: errors.InvalidAddressCheckSum('lemob')},
+        {
+            field: 'from',
+            configData: 'Lemo9A9JGWQT74H37PSB24RTH6YYHG6W3GCH3CJ8S',
+            error: errors.InvalidAddressLength('Lemo9A9JGWQT74H37PSB24RTH6YYHG6W3GCH3CJ8S'),
+        },
+        {field: 'gasPayer', configData: undefined, result: ''},
+        {field: 'gasPayer', configData: ''},
+        {field: 'gasPayer', configData: 0x1, error: errors.TXInvalidType('gasPayer', 0x1, ['string'])},
+        {field: 'gasPayer', configData: '0x1', result: 'Lemo8888888888888888888888888888888888BW', error: errors.InvalidAddress('0x1')},
+        {field: 'gasPayer', configData: 'lemobw'},
+        {field: 'gasPayer', configData: 'lemob', error: errors.InvalidAddressCheckSum('lemob')},
+        {
+            field: 'gasPayer',
+            configData: 'Lemo9A9JGWQT74H37PSB24RTH6YYHG6W3GCH3CJ8S',
+            error: errors.InvalidAddressLength('Lemo9A9JGWQT74H37PSB24RTH6YYHG6W3GCH3CJ8S'),
+        },
     ]
     tests.forEach(test => {
         it(`set ${test.field} to ${JSON.stringify(test.configData)}`, () => {
@@ -237,10 +335,13 @@ describe('all config', () => {
                 }, test.error)
             } else {
                 const tx = new Tx(config)
-                if (typeof test.result !== 'undefined') {
-                    assert.deepStrictEqual(tx[test.field], test.result)
-                } else {
+                if (typeof test.result === 'undefined') {
                     assert.deepStrictEqual(tx[test.field], test.configData)
+                } else if (test.field === 'expirationTime') {
+                    assert.isAtLeast(tx.expirationTime, test.result)
+                    assert.isAtMost(tx.expirationTime, test.result + 2)
+                } else {
+                    assert.deepStrictEqual(tx[test.field], test.result)
                 }
             }
         })
